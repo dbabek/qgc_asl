7bd30e60ae1b172106f0274000632ba42b720806
philipoe <philipp.oettershagen@mavt.ethz.ch>
2013-09-04 20:16:22 +0200

    Added adc121_cs2_amp to custom_sensor_data mavlink message

--- libs/mavlink/include/mavlink/v1.0/ASLUAV/mavlink_msg_custom_sensor_data.h
+++ libs/mavlink/include/mavlink/v1.0/ASLUAV/mavlink_msg_custom_sensor_data.h
@@ -10,22 +10,27 @@ typedef struct __mavlink_custom_sensor_data_t
  float adc121_vspb_volt; ///< power board voltage sensor reading in volts
  float adc121_cspb_amp; ///< power board current sensor reading in amps
  float adc121_cs1_amp; ///< board current sensor 1 reading in amps
+ float adc121_cs2_amp; ///< board current sensor 2 reading in amps
 } mavlink_custom_sensor_data_t;
 
-#define MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN 24
-#define MAVLINK_MSG_ID_202_LEN 24
+#define MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN 28
+#define MAVLINK_MSG_ID_202_LEN 28
+
+#define MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_CRC 63
+#define MAVLINK_MSG_ID_202_CRC 63
 
 
 
 #define MAVLINK_MESSAGE_INFO_CUSTOM_SENSOR_DATA { \
 	"CUSTOM_SENSOR_DATA", \
-	6, \
+	7, \
 	{  { "dbaro_pres_pa", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_custom_sensor_data_t, dbaro_pres_pa) }, \
          { "dbaro_velo_ms", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_custom_sensor_data_t, dbaro_velo_ms) }, \
          { "amb_temp_celsius", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_custom_sensor_data_t, amb_temp_celsius) }, \
          { "adc121_vspb_volt", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_custom_sensor_data_t, adc121_vspb_volt) }, \
          { "adc121_cspb_amp", NULL, MAVLINK_TYPE_FLOAT, 0, 16, offsetof(mavlink_custom_sensor_data_t, adc121_cspb_amp) }, \
          { "adc121_cs1_amp", NULL, MAVLINK_TYPE_FLOAT, 0, 20, offsetof(mavlink_custom_sensor_data_t, adc121_cs1_amp) }, \
+         { "adc121_cs2_amp", NULL, MAVLINK_TYPE_FLOAT, 0, 24, offsetof(mavlink_custom_sensor_data_t, adc121_cs2_amp) }, \
          } \
 }
 
@@ -42,21 +47,23 @@ typedef struct __mavlink_custom_sensor_data_t
  * @param adc121_vspb_volt power board voltage sensor reading in volts
  * @param adc121_cspb_amp power board current sensor reading in amps
  * @param adc121_cs1_amp board current sensor 1 reading in amps
+ * @param adc121_cs2_amp board current sensor 2 reading in amps
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_custom_sensor_data_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-						       float dbaro_pres_pa, float dbaro_velo_ms, float amb_temp_celsius, float adc121_vspb_volt, float adc121_cspb_amp, float adc121_cs1_amp)
+						       float dbaro_pres_pa, float dbaro_velo_ms, float amb_temp_celsius, float adc121_vspb_volt, float adc121_cspb_amp, float adc121_cs1_amp, float adc121_cs2_amp)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[24];
+	char buf[MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN];
 	_mav_put_float(buf, 0, dbaro_pres_pa);
 	_mav_put_float(buf, 4, dbaro_velo_ms);
 	_mav_put_float(buf, 8, amb_temp_celsius);
 	_mav_put_float(buf, 12, adc121_vspb_volt);
 	_mav_put_float(buf, 16, adc121_cspb_amp);
 	_mav_put_float(buf, 20, adc121_cs1_amp);
+	_mav_put_float(buf, 24, adc121_cs2_amp);
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 24);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
 #else
 	mavlink_custom_sensor_data_t packet;
 	packet.dbaro_pres_pa = dbaro_pres_pa;
@@ -65,19 +72,24 @@ static inline uint16_t mavlink_msg_custom_sensor_data_pack(uint8_t system_id, ui
 	packet.adc121_vspb_volt = adc121_vspb_volt;
 	packet.adc121_cspb_amp = adc121_cspb_amp;
 	packet.adc121_cs1_amp = adc121_cs1_amp;
+	packet.adc121_cs2_amp = adc121_cs2_amp;
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 24);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
 #endif
 
 	msg->msgid = MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA;
-	return mavlink_finalize_message(msg, system_id, component_id, 24, 63);
+#if MAVLINK_CRC_EXTRA
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_CRC);
+#else
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
+#endif
 }
 
 /**
  * @brief Pack a custom_sensor_data message on a channel
  * @param system_id ID of this system
  * @param component_id ID of this component (e.g. 200 for IMU)
- * @param chan The MAVLink channel this message was sent over
+ * @param chan The MAVLink channel this message will be sent over
  * @param msg The MAVLink message to compress the data into
  * @param dbaro_pres_pa Differential pressure, already temp. comp.
  * @param dbaro_velo_ms Velocity calculation from dpressure sensor	in m/sec
@@ -85,22 +97,24 @@ static inline uint16_t mavlink_msg_custom_sensor_data_pack(uint8_t system_id, ui
  * @param adc121_vspb_volt power board voltage sensor reading in volts
  * @param adc121_cspb_amp power board current sensor reading in amps
  * @param adc121_cs1_amp board current sensor 1 reading in amps
+ * @param adc121_cs2_amp board current sensor 2 reading in amps
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_custom_sensor_data_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
 							   mavlink_message_t* msg,
-						           float dbaro_pres_pa,float dbaro_velo_ms,float amb_temp_celsius,float adc121_vspb_volt,float adc121_cspb_amp,float adc121_cs1_amp)
+						           float dbaro_pres_pa,float dbaro_velo_ms,float amb_temp_celsius,float adc121_vspb_volt,float adc121_cspb_amp,float adc121_cs1_amp,float adc121_cs2_amp)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[24];
+	char buf[MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN];
 	_mav_put_float(buf, 0, dbaro_pres_pa);
 	_mav_put_float(buf, 4, dbaro_velo_ms);
 	_mav_put_float(buf, 8, amb_temp_celsius);
 	_mav_put_float(buf, 12, adc121_vspb_volt);
 	_mav_put_float(buf, 16, adc121_cspb_amp);
 	_mav_put_float(buf, 20, adc121_cs1_amp);
+	_mav_put_float(buf, 24, adc121_cs2_amp);
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, 24);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
 #else
 	mavlink_custom_sensor_data_t packet;
 	packet.dbaro_pres_pa = dbaro_pres_pa;
@@ -109,16 +123,21 @@ static inline uint16_t mavlink_msg_custom_sensor_data_pack_chan(uint8_t system_i
 	packet.adc121_vspb_volt = adc121_vspb_volt;
 	packet.adc121_cspb_amp = adc121_cspb_amp;
 	packet.adc121_cs1_amp = adc121_cs1_amp;
+	packet.adc121_cs2_amp = adc121_cs2_amp;
 
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, 24);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
 #endif
 
 	msg->msgid = MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA;
-	return mavlink_finalize_message_chan(msg, system_id, component_id, chan, 24, 63);
+#if MAVLINK_CRC_EXTRA
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_CRC);
+#else
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
+#endif
 }
 
 /**
- * @brief Encode a custom_sensor_data struct into a message
+ * @brief Encode a custom_sensor_data struct
  *
  * @param system_id ID of this system
  * @param component_id ID of this component (e.g. 200 for IMU)
@@ -127,7 +146,21 @@ static inline uint16_t mavlink_msg_custom_sensor_data_pack_chan(uint8_t system_i
  */
 static inline uint16_t mavlink_msg_custom_sensor_data_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_custom_sensor_data_t* custom_sensor_data)
 {
-	return mavlink_msg_custom_sensor_data_pack(system_id, component_id, msg, custom_sensor_data->dbaro_pres_pa, custom_sensor_data->dbaro_velo_ms, custom_sensor_data->amb_temp_celsius, custom_sensor_data->adc121_vspb_volt, custom_sensor_data->adc121_cspb_amp, custom_sensor_data->adc121_cs1_amp);
+	return mavlink_msg_custom_sensor_data_pack(system_id, component_id, msg, custom_sensor_data->dbaro_pres_pa, custom_sensor_data->dbaro_velo_ms, custom_sensor_data->amb_temp_celsius, custom_sensor_data->adc121_vspb_volt, custom_sensor_data->adc121_cspb_amp, custom_sensor_data->adc121_cs1_amp, custom_sensor_data->adc121_cs2_amp);
+}
+
+/**
+ * @brief Encode a custom_sensor_data struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param custom_sensor_data C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_custom_sensor_data_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_custom_sensor_data_t* custom_sensor_data)
+{
+	return mavlink_msg_custom_sensor_data_pack_chan(system_id, component_id, chan, msg, custom_sensor_data->dbaro_pres_pa, custom_sensor_data->dbaro_velo_ms, custom_sensor_data->amb_temp_celsius, custom_sensor_data->adc121_vspb_volt, custom_sensor_data->adc121_cspb_amp, custom_sensor_data->adc121_cs1_amp, custom_sensor_data->adc121_cs2_amp);
 }
 
 /**
@@ -140,21 +173,27 @@ static inline uint16_t mavlink_msg_custom_sensor_data_encode(uint8_t system_id, 
  * @param adc121_vspb_volt power board voltage sensor reading in volts
  * @param adc121_cspb_amp power board current sensor reading in amps
  * @param adc121_cs1_amp board current sensor 1 reading in amps
+ * @param adc121_cs2_amp board current sensor 2 reading in amps
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_custom_sensor_data_send(mavlink_channel_t chan, float dbaro_pres_pa, float dbaro_velo_ms, float amb_temp_celsius, float adc121_vspb_volt, float adc121_cspb_amp, float adc121_cs1_amp)
+static inline void mavlink_msg_custom_sensor_data_send(mavlink_channel_t chan, float dbaro_pres_pa, float dbaro_velo_ms, float amb_temp_celsius, float adc121_vspb_volt, float adc121_cspb_amp, float adc121_cs1_amp, float adc121_cs2_amp)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	char buf[24];
+	char buf[MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN];
 	_mav_put_float(buf, 0, dbaro_pres_pa);
 	_mav_put_float(buf, 4, dbaro_velo_ms);
 	_mav_put_float(buf, 8, amb_temp_celsius);
 	_mav_put_float(buf, 12, adc121_vspb_volt);
 	_mav_put_float(buf, 16, adc121_cspb_amp);
 	_mav_put_float(buf, 20, adc121_cs1_amp);
+	_mav_put_float(buf, 24, adc121_cs2_amp);
 
-	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, buf, 24, 63);
+#if MAVLINK_CRC_EXTRA
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, buf, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_CRC);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, buf, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
+#endif
 #else
 	mavlink_custom_sensor_data_t packet;
 	packet.dbaro_pres_pa = dbaro_pres_pa;
@@ -163,8 +202,13 @@ static inline void mavlink_msg_custom_sensor_data_send(mavlink_channel_t chan, f
 	packet.adc121_vspb_volt = adc121_vspb_volt;
 	packet.adc121_cspb_amp = adc121_cspb_amp;
 	packet.adc121_cs1_amp = adc121_cs1_amp;
+	packet.adc121_cs2_amp = adc121_cs2_amp;
 
-	_mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, (const char *)&packet, 24, 63);
+#if MAVLINK_CRC_EXTRA
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, (const char *)&packet, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_CRC);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA, (const char *)&packet, MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
+#endif
 #endif
 }
 
@@ -234,6 +278,16 @@ static inline float mavlink_msg_custom_sensor_data_get_adc121_cs1_amp(const mavl
 }
 
 /**
+ * @brief Get field adc121_cs2_amp from custom_sensor_data message
+ *
+ * @return board current sensor 2 reading in amps
+ */
+static inline float mavlink_msg_custom_sensor_data_get_adc121_cs2_amp(const mavlink_message_t* msg)
+{
+	return _MAV_RETURN_float(msg,  24);
+}
+
+/**
  * @brief Decode a custom_sensor_data message into a struct
  *
  * @param msg The message to decode
@@ -248,7 +302,8 @@ static inline void mavlink_msg_custom_sensor_data_decode(const mavlink_message_t
 	custom_sensor_data->adc121_vspb_volt = mavlink_msg_custom_sensor_data_get_adc121_vspb_volt(msg);
 	custom_sensor_data->adc121_cspb_amp = mavlink_msg_custom_sensor_data_get_adc121_cspb_amp(msg);
 	custom_sensor_data->adc121_cs1_amp = mavlink_msg_custom_sensor_data_get_adc121_cs1_amp(msg);
+	custom_sensor_data->adc121_cs2_amp = mavlink_msg_custom_sensor_data_get_adc121_cs2_amp(msg);
 #else
-	memcpy(custom_sensor_data, _MAV_PAYLOAD(msg), 24);
+	memcpy(custom_sensor_data, _MAV_PAYLOAD(msg), MAVLINK_MSG_ID_CUSTOM_SENSOR_DATA_LEN);
 #endif
 }
